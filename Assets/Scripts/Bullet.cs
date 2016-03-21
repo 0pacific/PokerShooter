7d5d41fe5f8cad6422283d71ddad6b813a47462c
Taihei Ogitani <joetsu.saiko@gmail.com>
2016-03-21 18:05:20 +0900

    Move and shot

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+  //弾丸の動きなど
+
+  public float speed; //弾丸の速度
+  public float vanishDistance; //弾丸が消滅する距離(Z軸方向)
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    //弾丸の移動
+    Vector3 moveVec = new Vector3(0.0f, 0.0f, speed * Time.deltaTime);
+
+    transform.position += moveVec;
+
+    //弾丸の消去
+    if(transform.position.z > vanishDistance)
+      Destroy(this.gameObject);
+
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+  // 自機の動きなど
+
+
+  Vector3 moveVec = Vector3.zero; //自機の移動ベクトル
+
+  public float width; // 動ける幅（左右均一）
+  public float speed; // 移動速度
+  public float shootDuration; // 弾丸の発射間隔
+  public GameObject bulletPrefab; // 弾丸のプレファブ
+
+	// Use this for initialization
+	void Start () {
+
+    // コルーチンで通常弾を常に発射させる
+    StartCoroutine(Shoot());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    // 移動関連
+    moveVec = Vector3.zero;
+
+    if(Input.GetKey("right"))
+      MoveRight();
+    else if(Input.GetKey("left"))
+      MoveLeft();
+
+    // ボール発射
+    if(Input.GetKeyDown("space"))
+      ShootBall();
+
+    // 移動実行
+    transform.position = transform.position + moveVec;
+  
+	}
+
+  void MoveRight ()
+  {
+    if(transform.position.x > width)
+      return;
+    
+    moveVec.x = speed * Time.deltaTime;
+  }
+
+  void MoveLeft ()
+  {
+    if(transform.position.x < -1 * width)
+      return;
+    
+    moveVec.x = -1 * speed * Time.deltaTime;
+  }
+
+  void ShootBall ()
+  {
+    Debug.Log("ShootBall");
+  }
+
+  IEnumerator Shoot ()
+  {
+    while(true)
+    {
+      // shootDuration毎に弾丸を発射
+      yield return new WaitForSeconds(shootDuration);
+      GameObject bullet = (GameObject)Instantiate(
+        bulletPrefab,
+        transform.position,
+        Quaternion.identity
+      );
+    }
+
+  }
+}
