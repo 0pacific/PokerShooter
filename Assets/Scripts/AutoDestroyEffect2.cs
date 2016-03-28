4ff72ceb1e6e1837936ab83f95f23f37ebb239c4
shirhxio <shirhxio537@gmail.com>
2016-03-28 09:39:30 +0900

    [add]爆発パーティクル追加

---
+++ Assets/Scripts/AutoDestroyEffect2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+// パーティクルを自動消去（Loopなしのみ)
+public class AutoDestroyEffect2 : MonoBehaviour {
+
+	void Start () {
+		// Loopしないパーティクルのみ
+		ParticleSystem particle = GetComponent<ParticleSystem> ();
+		Destroy (this.gameObject, particle.duration);
+	}
+}
