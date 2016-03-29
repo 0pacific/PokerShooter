af1016266a7a67afb3d46a4520226ef648b9c22d
shirhxio <shirhxio537@gmail.com>
2016-03-29 21:10:44 +0900

    [add]EnemyMovingCircle.csを追加
    敵の動きの種類を追加（円運動）。♣︎弾の調整。

---
+++ Assets/Scripts/Enemy/EnemyMoving/EnemyMovingCircle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovingCircle : MonoBehaviour {
+	[SerializeField]
+	private float period = 3;
+	[SerializeField]
+	private float radius = 1.5f;
+
+	private Vector3 center;
+
+	private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		center = transform.position - Vector3.forward * radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float t = 2 * Mathf.PI * timer / period;
+		float x = radius * Mathf.Sin (t);
+		float z = radius * Mathf.Cos (t);
+		transform.position = new Vector3 (center.x + x, 0, center.z + z);
+	}
+}
